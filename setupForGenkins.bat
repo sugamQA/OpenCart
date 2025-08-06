d6d03fd3bfab687d134cdd136395837714912683
sugam Chhetri <sugam.chhetri@acme.edu.np>
2025-08-06 14:53:33 +0545

    updated

--- setupForGenkins.bat
+++ setupForGenkins.bat
@@ -2,4 +2,4 @@ npm install
 npm install -D allure-playwright
 npm install -g allure-commandline --force
 npx playwright install
-npm install --omit=dev --legacy-peer-deps || echo "Ignoring install errors"
+npm install --no-audit
