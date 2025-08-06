e936ff86d5d6bea529a8221cb2efc54ac52ed43a
sugam Chhetri <sugam.chhetri@acme.edu.np>
2025-08-06 07:59:49 +0545

    OpenCart commit

---
+++ test.config.ts
@@ -0,0 +1,16 @@
+export class TestConfig{
+ 
+   //appUrl="http://localhost/opencart/upload/"
+    //appUrl="https://naveenautomationlabs.com/opencart"
+    appUrl="https://tutorialsninja.com/demo/"
+    
+  
+    //valid login credentials
+    email="Filterdemo@gmail.com"
+    password="123456"
+
+    //product details
+    productName="MacBook"
+    productQuantity="2"
+    totalPrice="$1,204.00"
+}
